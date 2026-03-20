b66eee0c0d04cb0c825e7a3f46d32218d4a5edbd
danielllucas000 <danielluccas000@gmail.com>
2026-03-20 13:01:53 -0300

    feat: implement user registration use case with Prisma repository

--- src/use-cases/register.ts
+++ src/use-cases/register.ts
@@ -1,1 +1,34 @@
-export class RegisterUseCase {}
+import type { UsersRepository } from "@/repositories/users-repository.js";
+import type { User } from "@prisma/client";
+import { hash } from "bcryptjs";
+
+interface RegisterUserCaseRequest {
+    name: string
+    email: string
+    password: string
+}
+
+interface RegisterUseCaseResponse {
+    user: User
+}
+export class RegisterUseCase {
+    constructor(private usersRepository: UsersRepository) {}
+
+    async execute({ name, email, password }: RegisterUserCaseRequest): Promise<RegisterUseCaseResponse> {
+        const password_hash = await hash(password, 6)
+
+        const userWithSameEmail = await this.usersRepository.findByEmail(email)
+
+        if(userWithSameEmail) {
+            throw new Error()
+        }
+
+        const user = await this.usersRepository.create({
+            name,
+            email,
+            password: password_hash
+        })
+
+        return { user }
+    } 
+}
